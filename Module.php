40aa964b5dd38a5a2b3b48a4899a925cbfd550ad
Matt Saladna <msaladna@apisnetworks.com>
2024-04-12 09:45:50 -0400

    Keyring support

--- Module.php
+++ Module.php
@@ -12,11 +12,14 @@
 
 	use GuzzleHttp\Exception\ClientException;
 	use Module\Provider\Contracts\ProviderInterface;
+	use Opcenter\Crypto\Keyring;
+	use Opcenter\Crypto\KeyringTrait;
 	use Opcenter\Dns\Record as BaseRecord;
 
 	class Module extends \Dns_Module implements ProviderInterface
 	{
 		use \NamespaceUtilitiesTrait;
+		use KeyringTrait;
 
 		/**
 		 * apex markers are marked with @
@@ -41,6 +44,7 @@
 		public const SHOW_NS_APEX = false;
 
 		protected $metaCache = [];
+
 		// @var array API credentials
 		private $key;
 
@@ -48,6 +52,10 @@
 		{
 			parent::__construct();
 			$this->key = $this->getServiceValue('dns', 'key', DNS_PROVIDER_KEY);
+
+			if (Keyring::is($this->key)) {
+				$this->key = $this->readKeyringValue($this->key);
+			}
 		}
 
 		/**
